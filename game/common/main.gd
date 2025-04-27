d871d3c4e33a76feaf28f5f6bab4df4c3dc5ce0a
Elfiawesome23 <elfiawesome23@gmail.com>
2025-04-27 19:36:48 +0800

    Added testing httpreq

--- game/common/main.gd
+++ game/common/main.gd
@@ -1,1 +1,18 @@
 extends Node
+
+const ENTRYPOINT := "https://raw.githubusercontent.com/Elfiawesome/battle-things/refs/heads/main/server/"
+
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
+var http_req: HTTPRequest = HTTPRequest.new()
+
+func _ready() -> void:
+	add_child(http_req)
+	http_req.request_completed.connect(_on_request_completed)
+	http_req.request(ENTRYPOINT + "asset/leaf.png")
+
+func _on_request_completed(result: int, response_code: int, headers: PackedStringArray, body: PackedByteArray) -> void:
+	var image := Image.new()
+	image.load_png_from_buffer(body)
+	var image_texture := ImageTexture.create_from_image(image)
+	sprite_2d.texture = image_texture
